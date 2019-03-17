64de6fc62116aaadb2453292ed00131a3ef9d136
danielmihai4 <danielmihai4@yahoo.com>
2019-03-17 18:07:25 +0000

    fix migration

--- db/migrate/20180811214547_create_list_item.rb
+++ db/migrate/20180811214547_create_list_item.rb
@@ -3,7 +3,7 @@ class CreateListItem < ActiveRecord::Migration[5.2]
     create_table :items do |t|
       t.string :name
       t.belongs_to :list
-      t.boolean :is_done, :null => false, :default => 0
+      t.boolean :is_done, :null => false, :default => false
       t.datetime :due_date
       t.timestamps
     end
